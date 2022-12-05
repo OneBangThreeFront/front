6ca52eee8310bc01523b4475ad71aa9db358c3b5
BeomSu <48756063+Seobeomsu@users.noreply.github.com>
2022-12-05 23:24:03 +0900

    chat 기능 만들긴했는데 순서가 이상해요..

--- Task/DnD.js
+++ Task/DnD.js
@@ -1,3 +1,68 @@
+const $address = 'http://ec2-43-201-47-225.ap-northeast-2.compute.amazonaws.com'
+
+
+
+window.onload=function(){
+	accessToken = localStorage.getItem('accessToken');
+	projectId = localStorage.getItem('projectid');
+
+	GetTaskData("TODO");
+	GetTaskData("IN_PROGRESS");
+	GetTaskData("RESOLVED");
+
+}
+
+function GetTaskData(Status){
+	var xhr = new XMLHttpRequest();
+  xhr.open("GET", $address+'/task/'+projectId+'?status='+Status, true);
+	xhr.setRequestHeader('Authorization',"Bearer " + accessToken);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState === xhr.DONE) {
+          if (xhr.status === 200) {
+            }
+            else {
+				      // 오류시 localStorage를 초기화하고 로그인화면으로
+							localStorage.clear();
+							location.href='./Login.html';
+            }
+        }
+    }
+  xhr.send();
+	xhr.onload = function(){
+		data = JSON.parse(xhr.responseText);
+		for(let i=0; i<data.length; i++){
+      // json 형식으로 가져온걸로 프로젝트 화면만들기~~
+        makediv(data[i].description, data[i].id, data[i].score, data[i].taskName,data[i].taskStatus)
+				dragStart();
+			}
+	}
+}
+
+function makediv(description,id,score,taskName,taskStatus){
+  //추가할 위치 설정
+  const element = document.getElementById(taskStatus);
+	// div들 만들기
+  const newdiv = document.createElement('div');
+	newdiv.setAttribute('draggable','true');
+	newdiv.classList.add('list-item');
+  // div마다 task id 설정
+	newdiv.id=id
+	
+	const newdescription = document.createElement('div');
+	const newscore = document.createElement('div');
+	const newtaskName = document.createElement('div');
+
+	newdescription.append(document.createTextNode(description));
+	newscore.append(document.createTextNode(score));
+	newtaskName.append(document.createTextNode(taskName));
+
+	newdiv.append(newtaskName,newdescription,newscore);
+
+  element.append(newdiv);
+}
+
+function dragStart(){
 const list_items = document.querySelectorAll('.list-item');
 const lists = document.querySelectorAll('.list');
 
@@ -5,6 +70,8 @@ let draggedItem = null;
 
 for (let i = 0; i < list_items.length; i++) {
 	const item = list_items[i];
+
+	console.log(item);
 
 	item.addEventListener('dragstart', function () {
 		draggedItem = item;
@@ -43,67 +110,4 @@ for (let i = 0; i < list_items.length; i++) {
 		});
 	}
 }
-
-
-
-const $address = 'http://ec2-43-201-47-225.ap-northeast-2.compute.amazonaws.com'
-
-
-
-window.onload=function(){
-	accessToken = localStorage.getItem('accessToken');
-	projectId = localStorage.getItem('projectid');
-
-	GetTaskData("TODO");
-	GetTaskData("IN_PROGRESS");
-	GetTaskData("RESOLVED");
-
 }
-
-function GetTaskData(Status){
-	var xhr = new XMLHttpRequest();
-  xhr.open("GET", $address+'/task/'+projectId+'?status='+Status, true);
-	xhr.setRequestHeader('Authorization',"Bearer " + accessToken);
-  xhr.setRequestHeader('Content-Type', 'application/json');
-  xhr.onreadystatechange = function () {
-      if (xhr.readyState === xhr.DONE) {
-          if (xhr.status === 200) {
-            }
-            else {
-				      // 오류시 localStorage를 초기화하고 로그인화면으로
-							localStorage.clear();
-							location.href='./Login.html';
-            }
-        }
-    }
-  xhr.send();
-	xhr.onload = function(){
-		data = JSON.parse(xhr.responseText);
-		for(let i=0; i<data.length; i++){
-      // json 형식으로 가져온걸로 프로젝트 화면만들기~~
-        makediv(data[i].description, data[i].id, data[i].score, data[i].taskName,data[i].taskStatus)
-    }
-	}
-}
-
-function makediv(description,id,score,taskName,taskStatus){
-  //추가할 위치 설정
-  const element = document.getElementById(taskStatus);
-	// div들 만들기
-  const newdiv = document.createElement('div');
-  // div마다 task id 설정
-	newdiv.id=id
-	
-	const newdescription = document.createElement('div');
-	const newscore = document.createElement('div');
-	const newtaskName = document.createElement('div');
-
-	newdescription.append(document.createTextNode(description));
-	newscore.append(document.createTextNode(score));
-	newtaskName.append(document.createTextNode(taskName));
-
-	newdiv.append(newtaskName,newdescription,newscore);
-
-  element.append(newdiv);
-}
-
